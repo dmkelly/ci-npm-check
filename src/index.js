84566a2addd1776380038d52597548d2c1a47e2c
Dave Kelly <dkelly@brightcove.com>
2016-10-04 13:31:53 -0400

    Write warnings to console if outdated packages are found

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const npmCheck = require('npm-check');
+const chalk = require('chalk');
+
+const PATCH = 'patch';
+
+function warn (...args) {
+  global.console.warn(chalk.bold.yellow('WARN'), ...args);
+}
+
+function run () {
+  npmCheck().then((currentState) => {
+    const packages = currentState.get('packages');
+    let hasWarnings = false;
+    packages.forEach(pkg => {
+      const { moduleName, isInstalled, installed, latest, bump, unused } = pkg;
+      if (isInstalled && installed !== latest && bump !== PATCH) {
+        warn(`${moduleName}@${installed} is outdated. Latest is ${latest}`);
+        hasWarnings = true;
+      }
+      if (unused) {
+        hasWarnings = true;
+        warn(`${moduleName} is unused`);
+      }
+    });
+
+    if (hasWarnings) {
+      global.console.info('Exited with WARNINGS');
+    }
+  });
+}
+
+run();
